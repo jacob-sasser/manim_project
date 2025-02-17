20219a81bef2601ba745af9a3fcbbf5ea0251866
jacob-sasser <jacobsasser23@gmail.com>
2025-02-17 10:23:43 -0500

    input box

--- FrontendApp.py
+++ FrontendApp.py
@@ -1,12 +1,22 @@
 import pyglet
 from pyglet import shapes
+import os
+
+user_path=pyglet.resource.get_data_path('InteractiveManim/configs')
+if not os.path.exists(user_path): os.makedirs(user_path)
+print(user_path)
 
 
-
-window=pyglet.window.Window(800,600,"Test")
+window=pyglet.window.Window(800,600,"Manim interaction tests")
 
 start_x,start_y=0,0
-
+#Simple test input box for saving to json
+initial_input_box=pyglet.gui.TextEntry(
+text="Enter node",
+x=100,
+y=100,
+width=50
+)
 @window.event
 def on_mouse_press(x,y,button,modifiers):
     global dragging,start_x,start_y 
@@ -22,6 +32,9 @@ def on_mouse_drag(x,y,dx,dy,button,modifiers):
     global dragging,start_y,start_x
     if dragging:
         pass #Fix
+@window.event('initial_input_box')
+def show_text_box():
+    initial_input_box.event()
 def run_app(): 
     pyglet.app.run()
 
