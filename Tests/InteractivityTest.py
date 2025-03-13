d57ee4f3a5e86f34fd3b05c6b3721d87b4111d1b
jacob-sasser <jacobsasser23@gmail.com>
2025-03-12 23:17:56 -0400

    Initial zoom on mouse click.

--- Tests/InteractivityTest.py
+++ Tests/InteractivityTest.py
@@ -4,10 +4,16 @@ from manim.opengl import *
 import pyglet
 import time
 import random
-
+from pyglet.window import mouse
 
 
 class GraphTemp2(MovingCameraScene):
+    locations={}
+    def get_key(self, x, y, z):
+        for key, value in self.pos.items():
+            if value == (x, y, z):
+                return key
+        return None
     def construct(self):
         start = time.time()
         #Variable n defines number of nodes.
@@ -26,6 +32,7 @@ class GraphTemp2(MovingCameraScene):
             edges.append(c2)
         #Randomizing the position of nodes.
         self.pos = {i: (random.randint(-2,2), random.randint(-2,2), 0) for i in range(n)}
+        self.locations=self.pos
         #Creating graph mobject.
         self.G = Graph(
             nodes, 
@@ -41,7 +48,8 @@ class GraphTemp2(MovingCameraScene):
         self.add(self.G)
         end = time.time()
         
-        print(f"Time taken: {(end-start)*10**3:.03f}ms")       
+        print(f"Time taken: {(end-start)*10**3:.03f}ms")  
+        print(self.pos)     
         self.interactive_embed()
     def on_key_press(self,symbol,modifiers):
             if symbol == pyglet.window.key._1:
@@ -53,6 +61,25 @@ class GraphTemp2(MovingCameraScene):
                 )
             super().on_key_press(symbol, modifiers)
 
+    def on_mouse_press(self, point, button, modifiers):
+            reset=True
+            x,y,z=point
+            rounded_x = round(x, 0)
+            rounded_y = round(y, 0)
+            if rounded_x==-0.0:
+                 rounded_x=0.
+            if rounded_y == -0.0:
+                 rounded_y=0.
+            print(rounded_x,rounded_y)
+            
+            for node, pos in self.pos.items():
+                    
+                    if (round(pos[0], 1), round(pos[1], 1)) == (rounded_x, rounded_y):
+                        self.play(
+                        self.camera.animate.scale(0.5).move_to(self.pos[node]),
+                        FadeOut(self.highlight),
+                        FadeIn(Circle(radius=0.25).move_to(self.pos[node]))
+                        )
 
-
+            super().on_mouse_press(point, button, modifiers)
 
