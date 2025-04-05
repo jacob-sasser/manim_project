9eef500a553448c249f6e4c715e1e3188bcbb53c
jacob-sasser <jacobsasser23@gmail.com>
2025-04-05 14:59:51 -0400

    Update Assignment.py

--- Assignment.py
+++ Assignment.py
@@ -1,35 +1,44 @@
 import pyglet
 from manim import *
-from manim.opengl import *
 import random
+from manim.opengl import *
 class Assignment():
     locations={}
     last_node=None
     correct_node = None
     incorrect_counter = 0
-    incorrect_max = 3
+    incorrect_max = 10
+    correct_counter=0
     question_text = None
     feedback_text = None  
+    options=[]
     assignments=[]
-    current_assignment_index=0
+    total_questions=len(assignments)
+    option_text=None
+    #grade=correct_node/total_questions
+    current_assignment_index=1
+
     def check_answer(self,node):
         if self.feedback_text:
             self.remove(self.feedback_text)
+            
         if node==self.correct_node:
+            
             self.feedback_text=Text("Correct").to_edge(DOWN)
             self.add(self.feedback_text)
             self.is_assignment=False
 
-            self.play(FadeOut(self.feedback_text), FadeOut(self.question_text), run_time=0.5 )      
+            self.play(FadeOut(self.feedback_text), FadeOut(self.question_text.scale(0.5)), run_time=0.5 )      
             self.current_assignment_index+=1
+            
             self.start_next_assignment()
         else:
             self.incorrect_counter+=1
             if self.incorrect_counter>=self.incorrect_max:
-                self.feedback_text = Text("Too many incorrect attempts.").to_edge(DOWN)
+                self.feedback_text = Text("Too many incorrect attempts.").scale(0.25).to_corner(DL)
                 self.is_assignment=False
                 
-            else: self.feedback_text=Text(f"Incorrect ( {self.incorrect_counter}/{self.incorrect_max})").to_edge(DOWN)
+            else: self.feedback_text=Text(f"Incorrect ( {self.incorrect_counter}/{self.incorrect_max})").scale(0.25).to_corner(DL)
             self.add(self.feedback_text)
 
     def start_next_assignment(self):
@@ -49,19 +58,14 @@ class Assignment():
             self.remove(self.question_text)
         if self.feedback_text:
             self.remove(self.feedback_text)
-        self.question_text = Text(question).to_edge(UP)
-        self.add(self.question_text.scale(0.4))
+        self.question_text = Text(question).to_edge(UP*1.5).scale(0.5)
+        self.add(self.question_text)
         self.generate_options(all_nodes)
         
     def complete_all_assignments(self):
         self.clear()
-        completion_text=Text("All assignments completed").scale(0.5)
+        completion_text=Text("All assignments completed")
         self.add(completion_text)
-    
-    def failed_assignment(self):
-        self.clear()
-        fail_text = Text("Too many incorrect attempts.").scale(0.5)
-        self.add(fail_text)
 
     def calculate_relative_node_pos(self,nodes,scale):
         camera_center=np.array(self.camera.get_center())
@@ -79,8 +83,8 @@ class Assignment():
             for txt in self.options:
                 self.remove(txt)
         incorrect_nodes = [node for node in all_nodes if node not in self.correct_node]
-        chosen_incorrect = random.sample(incorrect_nodes, min(4 - len(self.correct_node), len(incorrect_nodes)))
-        choices=self.correct_node+chosen_incorrect
+        self.chosen_incorrect = random.sample(incorrect_nodes, min(4 - len(self.correct_node), len(incorrect_nodes)))
+        choices=self.correct_node+self.chosen_incorrect
         random.shuffle(choices)
         option_labels=["A","B","C","D"]
         self.option_map = {option_labels[i]: choices[i] for i in range(len(choices))}
